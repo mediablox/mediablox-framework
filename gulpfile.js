44567daee595e591af9dd8fee5ff8b86b752bdb9
Dean Morgan <deanmorgan@mediablox.co.uk>
2020-03-19 01:11:14 +0000

    Added metalsmith build system for docs

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,11 @@ var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sass = require('gulp-sass');
-var sassdoc = require('sassdoc');
-
 
 gulp.task('scss', function(done) {
 	gulp.src('mbfw.scss')
 		.pipe(sourcemaps.init())
     .pipe(plumber())
-    .pipe(sassdoc())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(postcss([autoprefixer()])) // uses ".browserslistrc"
 		.pipe(sourcemaps.write('.'))
